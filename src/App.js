3dd3387901158f0d053b16af25a95e14120844a1
milimk <milimkim@gmail.com>
2024-09-10 14:00:52 -0700

    Adding Weather.js and linking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello React App</h1>
+        <Weather />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div>
+      <p>Weather App here</p>
+    </div>
+  );
+}
